341e361bf3fc21dfae8dd2310488d8c20187ee45
Mike Larah <mike.larah@endjin.com>
2014-09-22 18:16:47 +0100

    Added sample project for AngularJS unit testing

---
+++ AngularJsUnitTesting/Solutions/AngularJsUnitTesting/Specs/controllerSpecs.js
@@ -0,0 +1,39 @@
+﻿///<reference path="~/Scripts/jasmine/jasmine.js"/>
+///<reference path="~/Scripts/angular.js"/>
+///<reference path="~/Scripts/angular-mocks.js"/>
+
+///<reference path="~/App/app.js"/>
+///<reference path="~/App/controllers.js"/>
+///<reference path="~/App/Controllers/dogController.js"/>
+
+describe("Controllers", function() {
+
+    beforeEach(module("controllers"));
+
+    describe("Dog controller", function() {
+
+        var scope,
+            dog,
+            controller;
+
+        beforeEach(inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+
+            dog = {
+                query: function() {
+                    return [{ name: 'TEST', type: 'TEST' }];
+                }
+            }
+
+            controller = $controller('DogController', { $scope: scope, Dog: dog });
+        }));
+
+        it('should set the page title as "Dogs"', function() {
+            expect(scope.pageTitle).toBe('Dogs');
+        });
+
+        it('should have 1 dog', function () {
+            expect(scope.dogs.length).toBe(1);
+        });
+    });
+});
---
+++ AngularJsUnitTesting/Solutions/AngularJsUnitTesting/Specs/serviceSpecs.js
@@ -0,0 +1,30 @@
+﻿///<reference path="~/Scripts/jasmine/jasmine.js"/>
+///<reference path="~/Scripts/angular.js"/>
+///<reference path="~/Scripts/angular-mocks.js"/>
+
+///<reference path="~/App/app.js"/>
+///<reference path="~/App/services.js"/>
+///<reference path="~/App/Services/dogService.js"/>
+
+describe("Services", function() {
+
+    beforeEach(module("services"));
+
+    describe("Dog service", function() {
+
+        var dog;
+
+        beforeEach(inject(function($injector) {
+            dog = $injector.get('Dog');
+        }));
+
+        it('should return 3 dogs when querying', function() {
+            expect(dog.query().length).toBe(3);
+        });
+
+        it('should return 4 dogs when querying after adding a dog', function() {
+            dog.add({ name: 'Fido', type: 'German Shepherd' });
+            expect(dog.query().length).toBe(4);
+        });
+    });
+});
